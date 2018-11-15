e640fe22df052af3031d9e05bccd2a73126755b7
Ubuntu <azureops@table4msdevopsvm.uefdx4otlaiudodp45j3dglrlg.px.internal.cloudapp.net>
2018-11-15 21:18:54 +0000

    as

---
+++ modules/main.tf
@@ -0,0 +1,27 @@
+module "mycompute"{
+  source="Azure/compute/azurerm"
+  resource_group_name="${var.rg}"
+  location="${var.location}"
+  admin_password="Northwest1234!"
+  admin_username="azureops"
+  vm_os_simple="WindowsServer"
+  remote_port="3389"
+  nb_instances=2
+  public_ip_dns=["table4msapp"]
+  vnet_subnet_id="${module.network.vnet_subnets[0]}"
+}
+module "network"{
+  source="Azure/network/azurerm"
+  location="${var.location}"
+  resource_group_name="${var.rg}"
+}
+output "vm_public_name"{
+  value="${module.mycompute.public_ip_dns_name}"
+}
+output "vm_public_ip"{
+  value="${module.mycompute.public_ip_address}"
+}
+output "vm_private_ips"{
+  value="${module.mycompute.network_interface_private_ip}"
+}
+
---
+++ modules/var.tf
@@ -0,0 +1,20 @@
+variable "yo"
+{
+  type="string"
+  default="qewqwe"
+  description="Yo Val:"
+}
+variable "appid"{
+}
+variable "password"{
+}
+variable "sub_id"{
+}
+variable "tenant"{
+}
+variable "location"{
+}
+variable "rg"{
+}
+variable "sec_group_name"{
+}
